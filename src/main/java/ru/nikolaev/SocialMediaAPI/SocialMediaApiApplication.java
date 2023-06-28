f11faf3c18e01693d56f8595ee58e59ced4f638f
Alexey Nikolaev <nikolaev.alexey2004@gmail.com>
2023-06-28 18:42:08 +0300

    initial commit

---
+++ src/main/java/ru/nikolaev/SocialMediaAPI/SocialMediaApiApplication.java
@@ -0,0 +1,11 @@
+package ru.nikolaev.SocialMediaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaApiApplication.class, args);
+	}
+}
