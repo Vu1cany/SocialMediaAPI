c86dec2879eba10e4e9a11f50f16af90c382b801
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2023-09-20 21:10:56 +0300

    Update db-migration

--- src/main/resources/db-migration/v1.0/v1.0-dml-init.sql
+++ src/main/resources/db-migration/v1.0/v1.0-dml-init.sql
@@ -1,6 +1,7 @@
-INSERT INTO sm.users(username, email, password, created_at, disabled_at)
-VALUES ('test_username', 'test@mail.ru', 'test_password', now(), null);
+INSERT INTO sm.people(username, email, password, role, created_at, disabled_at)
+VALUES ('test_username', 'test@mail.ru', 'test_password', 'USER', now(), null);
 
-INSERT INTO sm.posts(user_id, image_link, text, created_at, updated_at, disabled_at)
-VALUES ((SELECT u.id FROM sm.users u WHERE u.username = 'test_username'), 'test', 'test', now(), now(), null),
-       ((SELECT u.id FROM sm.users u WHERE u.username = 'test_username'), 'test2', 'test', now(), now(), null);
+INSERT INTO sm.posts(person_id, image_link, text, created_at, updated_at, disabled_at)
+VALUES ((SELECT p.id FROM sm.people p WHERE p.username = 'test_username'), 'test', 'test', now(), now(), null),
+       ((SELECT p.id FROM sm.people p WHERE p.username = 'test_username'), 'test2', 'test2', now(), now(), null),
+       ((SELECT p.id FROM sm.people p WHERE p.username = 'test_username'), 'test3', 'test3', now(), now(), now());
